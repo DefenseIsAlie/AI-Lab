1d7266916846614035e9b413139a7cdc3e6e2e6b
karthik mv <karthik.murakonda14@gmail.com>
2021-12-28 12:19:06 +0530

    implemented prority queue

--- Lab-2/main.py
+++ Lab-2/main.py
@@ -10,28 +10,19 @@ class State:
     def __init__(self, grid):
         # store copy of grid
         self.grid = grid
+        self.parent = None
 
     def __str__(self) -> str:
         return f"{self.grid}"
+    
+    def __lt__(self, other):
+        return self.grid < other.grid
 
-with open(sys.argv[1], 'r') as f:
-    lines = f.readlines()
-    inputLine = []
-    for line in lines:
-        line = line.strip().split()
-        if line:
-            inputLine.append(line)
-    startState = inputLine[:3]
-    goalState = inputLine[-3:]
-    del inputLine
+    def __le__(self,other):
+        return self.grid <= other.grid
 
-start = State([["A", "E"], ["F"], ["B","D"]])
-goal = State([["A","B"],["D","E","F"],[]])
 
-State.stateHistory.append(start)
-
-def MoveGen(state):
-    neighbors = []
+def MoveGen(state, heuristic):
     # find the neighbors of the current state
     for i in range(len(state.grid)):
         if state.grid[i] != []:
@@ -42,11 +33,17 @@ def MoveGen(state):
             newState_1.grid[(i+1)%3].append(upperBlock)
             newState_2.grid[(i+2)%3].append(upperBlock)
             if newState_1.grid != state.grid and not any(newState_1.grid == x.grid for x in State.stateHistory):
-                neighbors.append(newState_1)
+                # find the heuristic value of the new state
+                h = heuristic(newState_1)
+                # add the new state to the priority queue
+                heapq.heappush(neighbors, (h, newState_1))
             if newState_2.grid != state.grid and not any(newState_2.grid == x.grid for x in State.stateHistory):
-                neighbors.append(newState_2)
-    return neighbors
-        
+                # find the heuristic value of the new state
+                h = heuristic(newState_2)
+                # add the new state to the priority queue
+                heapq.heappush(neighbors, (h, newState_2))
+    return
+
 def OrdHeuristic(s: State):
     g = goal
     ret = 0
@@ -58,9 +55,26 @@ def OrdHeuristic(s: State):
                 ret += abs(ord(j))
     return ret
 
+def heuristic():
+    pass
+
+with open(sys.argv[1], 'r') as f:
+    lines = f.readlines()
+    inputLine = []
+    for line in lines:
+        line = line.strip().split()
+        if line:
+            inputLine.append(line)
+    startState = inputLine[:3]
+    goalState = inputLine[-3:]
+    del inputLine
+start = State([["A", "E"], ["F"], ["B","D"]])
+goal = State([["A","B"],["D","E","F"],[]])
+
+State.stateHistory.append(start)
 
 
-listOfStates = MoveGen(start)
-for _D in listOfStates:
-    print(_D.grid)
-
+neighbors = []
+MoveGen(start, OrdHeuristic)
+for i in range(len(neighbors)):
+    print(neighbors[i][1].grid)
