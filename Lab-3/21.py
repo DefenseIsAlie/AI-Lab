a5b8314c85ce369393a82b0f504aaadc2f0b4bfa
M V Karthik <karthik.murakonda14@gmail.com>
2022-01-09 19:25:09 +0530

    added tabutenure in movegen

--- Lab-3/21.py
+++ Lab-3/21.py
@@ -33,7 +33,7 @@ def heuristic(state : list, formula : list) -> int:
             heuristic_value += 1
     return heuristic_value
 
-def stateNeighbors(state : list, bitflips : int) -> list:
+def stateNeighbors(state : list, bitflips : int, tabuTenureList : list= [], tabuTenure: int = 0) -> list:
     """
     Return the neighbors of a state. which are the possible states after
     flipping a bit.
@@ -43,8 +43,15 @@ def stateNeighbors(state : list, bitflips : int) -> list:
     for i in list(comb):
         neighbor = list(state)
         for j in i:
-            neighbor[j] = (neighbor[j]+1)%2
+            if tabuTenure and tabuTenureList[j] == 0:
+                neighbor[j] = (neighbor[j]+1)%2
+                tabuTenureList[j] = tabuTenure+1
+            elif tabuTenure == 0:
+                neighbor[j] = (neighbor[j]+1)%2
         neighbors.append(neighbor)
+    for i in range(tabuTenure):
+        if tabuTenure and tabuTenureList[i] > 0:
+            tabuTenure[i] -= 1
     return neighbors
 
 def movegen(state : list, stateList : list, Formula, number: int) -> list:
@@ -93,18 +100,39 @@ def variableNeighbor_descent(state : list, Formula) -> list:
     frontier = [state]
     statesExplored = []
     while frontier:
-        neighbor = stateNeighbors(frontier[0], bitflips)
-        if neighbor not in statesExplored:
-            if goalTest(neighbor):
-                return neighbor
-            else:
-                if heuristic(neighbor, Formula) < heuristic(frontier[0], Formula):
-                    frontier = [neighbor]
-                else:
-                    frontier.append(neighbor)
-        frontier.pop(0)
-            
+        current_state = frontier.pop()
+        statesExplored.append(current_state)
+        if goalTest(current_state, Formula):
+            return current_state, statesExplored
+        for i in  movegen(current_state, stateNeighbors(current_state, bitflips), Formula, 1):
+            if  heuristic(i, Formula) > heuristic(current_state, Formula) and i not in statesExplored and i not in frontier:
+                frontier.append(i)
+        while frontier == [] and bitflips < len(state)//2+1:
+            bitflips += 1
+            for i in  movegen(current_state, stateNeighbors(current_state, bitflips), Formula, 1):
+                if  heuristic(i, Formula) > heuristic(current_state, Formula) and i not in statesExplored and i not in frontier:
+                    frontier.append(i)
     return None
+
+def tabuSearch(state : list, Formula, tabuTenure : int) -> list:
+    """
+    Tabu search algorithm.
+    """
+    frontier = [state]
+    statesExplored = []
+    tabuTenureList = [0]*len(state)
+    while frontier:
+        current_state = frontier.pop()
+        statesExplored.append(current_state)
+        if goalTest(current_state, Formula):
+            return current_state, statesExplored
+        print(movegen(current_state, stateNeighbors(current_state, 1), Formula, 1, tabuTenureList, tabuTenure))
+        print(tabuTenureList)
+        for i in  movegen(current_state, stateNeighbors(current_state, 1), Formula, 1, tabuTenureList, tabuTenure):
+            if  heuristic(i, Formula) > heuristic(current_state, Formula) and i not in statesExplored and i not in frontier:
+                frontier.append(i)
+    return None, statesExplored
+    
 
 IntialState = [0,1,1,1,0]
 statesExplored = []
@@ -113,8 +141,5 @@ with open("clauses.txt") as f:
     for line in f:
         Formula.append(line.strip('\n').strip(']').strip('[').split(','))
 
-ans = BeamSearch(IntialState, 2, Formula)
-print(ans[0])
-print(ans[1])
-# ans = movegen([1,1,1,0,1], stateNeighbors([1,1,1,0,1], 1), Formula, 2)
-# print(ans)
+ans = variableNeighbor_descent(IntialState, Formula)
+print(ans)
